9e8c9a4ea06c4c616376114174b55e0cdf197b5d
najmiter <realnajmiter@gmail.com>
2024-06-16 22:44:48 +0500

    at least we're `eval`uating stuff now

--- src/main.js
+++ src/main.js
@@ -1,36 +1,72 @@
-const keys = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "+",
-    "-",
-    "%",
-    "/",
-    "*",
-];
+const digits = [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const operators = ["+", "-", "%", "/", "*"];
+
 const input = document.querySelector("#input");
+const keys = document.querySelector(".keys");
+const result = document.querySelector("#result");
+let hasEvaluated = false;
+
+input.addEventListener("keydown", (e) => e.preventDefault());
 
 document.addEventListener("keydown", (key) => {
     const { key: k } = key;
 
-    if (checkKey(k)) {
+    if (hasEvaluated && checkDigit(k)) startNewEvaluation();
+    if (hasEvaluated && checkOperator(k)) usePreviousResult();
+
+    if (k === "Escape") resetValues();
+
+    if (k === "Enter") {
+        result.textContent = eval(
+            input.value.replaceAll("x", "*").replaceAll("÷", "/")
+        );
+        hasEvaluated = true;
+    } else if (checkKey(k)) {
         input.value += processIntoKey(k);
     }
 });
 
+keys.addEventListener("click", (click) => {
+    const value = click.target.dataset.value;
+
+    if (hasEvaluated && checkDigit(value)) startNewEvaluation();
+    if (hasEvaluated && checkOperator(value)) usePreviousResult();
+
+    if (digits.includes(value) || operators.includes(value)) {
+        input.value += processIntoKey(value);
+    }
+});
+
+function checkDigit(key) {
+    return digits.includes(key);
+}
+
+function checkOperator(key) {
+    return operators.includes(key);
+}
+
 function checkKey(key) {
-    return keys.includes(key);
+    return digits.includes(key) || operators.includes(key);
 }
 
 function processIntoKey(key) {
     if (key === "*") return " x ";
     if (key === "/") return " ÷ ";
     return key;
+}
+
+function resetValues() {
+    input.value = "";
+    result.textContent = "0";
+}
+
+function startNewEvaluation() {
+    input.value = "";
+    hasEvaluated = false;
+}
+
+function usePreviousResult() {
+    input.value = result.textContent;
+    hasEvaluated = false;
 }
